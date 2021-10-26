f4ba5cb6c7b90ec5678d00036d7e6924229755ee
luismomm2110 <luismomm@gmail.com>
2021-10-26 14:07:38 -0300

    adapter.java

---
+++ design-patterns/week1/adapter.java
@@ -0,0 +1,34 @@
+public interface CoffeeMachineInterface {
+	void chooseFirstSelection();
+	void chooseSecondSelection();
+
+};
+
+public class OldCoffeeMachine {
+
+	void selectA() {};
+	void selectB() {};
+
+
+};
+
+public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+	private OldCoffeeMachine oldVendingMachine;
+
+	public CoffeeTouchscreenAdapter(OldCoffeeMachine oldVendingMachine) {
+		this.oldVendingMachine = oldVendingMachine;
+	} 
+
+	public void chooseFirstSelection() {
+		this.oldVendingMachine.selectA();
+	};
+
+	public void chooseSecondSelection() {
+		this.oldVendingMachine.selectB();
+	}
+
+
+
+
+
+};
